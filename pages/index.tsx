c036705c329f569b75357bb42e3235bd60269261
marin <mladovic.dev@gmail.com>
2022-10-03 11:50:54 +0200

    row and thumbnails

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import requests from '../utils/requests';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import Row from '../components/Row';
 import { Movie } from '../typings';
 
 interface Props {
@@ -26,7 +27,7 @@ const Home = ({
   trendingNow,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -35,12 +36,16 @@ const Home = ({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* ROW */}
-          {/* ROW */}
-          {/* ROW */}
-          {/* ROW */}
-          {/* ROW */}
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
 
